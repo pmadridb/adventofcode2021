624aa45f048098fe819581bd76a368221f75d703
Pedro Madrid <pedro.madrid@safefleet.net>
2021-12-02 22:51:37 -0400

    Day 2

---
+++ Day02.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Day02
+{
+    static void Main()
+    {
+        Part1();
+        Part2();
+    }
+
+    static void Part1()
+    {
+        string[] Lines = Util.GetLines("input02.txt");
+        int HorizontalPosition = 0, Depth = 0;
+        for (int i = 0; i < Lines.Length; i++)
+        {
+            var Instruction = Lines[i].Split(' ');
+            var Units = Int32.Parse(Instruction[1]);
+            switch (Instruction[0])
+            {
+                case "up":
+                    Depth -= Units;
+                    break;
+                case "down":
+                    Depth += Units;
+                    break;
+                case "forward":
+                    HorizontalPosition += Units;
+                    break;
+                default:
+                    break;
+            }
+        }
+        Console.WriteLine("Part one: " + Depth * HorizontalPosition);
+    }
+    static void Part2()
+    {
+        string[] Lines = Util.GetLines("input02.txt");
+        int HorizontalPosition = 0, Depth = 0, Aim = 0;
+        for (int i = 0; i < Lines.Length; i++)
+        {
+            var Instruction = Lines[i].Split(' ');
+            var Units = Int32.Parse(Instruction[1]);
+            switch (Instruction[0])
+            {
+                case "up":
+                    Aim -= Units;
+                    break;
+                case "down":
+                    Aim += Units;
+                    break;
+                case "forward":
+                    HorizontalPosition += Units;
+                    Depth += Aim * Units;
+                    break;
+                default:
+                    break;
+            }
+        }
+        Console.WriteLine("Part two: " + Depth * HorizontalPosition);
+
+    }
+}
---
+++ Util.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class Util
+{
+    public static string[] GetLines(string Filename)
+    {
+        return System.IO.File.ReadAllLines(@"c:\Users\pmadrid\Documents\GitHub\adventofcode2021\" + Filename);
+    }
+}
